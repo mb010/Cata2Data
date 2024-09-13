bb302603302316ab715679ed203d8cda7d4630ba
Oliver King <11923466+OllyK@users.noreply.github.com>
2024-09-13 16:44:23 +0100

    format: formatting fixes as defined by pre-commit hooks
    - closes #38

--- examples/lotssdr2/data_scrapper.py
+++ examples/lotssdr2/data_scrapper.py
@@ -6,17 +6,19 @@ from urllib.parse import urljoin
 import argparse
 from tqdm import tqdm
 
+
 # Function to get the full-resolution links from a single page
-def get_links(page_url, download_link='^public/DR2/mosaics/.*/mosaic-blanked.fits$'):
+def get_links(page_url, download_link="^public/DR2/mosaics/.*/mosaic-blanked.fits$"):
     response = requests.get(page_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    hrefs = soup.find_all('a', href=True)
+    soup = BeautifulSoup(response.content, "html.parser")
+    hrefs = soup.find_all("a", href=True)
     data_links = []
     for ref in hrefs:
-        if re.search(download_link, ref['href']):
-            data_links.append(ref['href'])
-    
+        if re.search(download_link, ref["href"]):
+            data_links.append(ref["href"])
+
     return data_links
+
 
 def download_all_files(links, base_url, test=False, dir="./data"):
     for link in tqdm(links):
@@ -27,7 +29,8 @@ def download_all_files(links, base_url, test=False, dir="./data"):
         if test:
             break
 
-def download_file(url, file_name, local_dir='./data', chunk_size=8192):
+
+def download_file(url, file_name, local_dir="./data", chunk_size=8192):
     full_url = urljoin(url, file_name)
 
     local_dir = Path(local_dir)
@@ -36,25 +39,44 @@ def download_file(url, file_name, local_dir='./data', chunk_size=8192):
 
     with requests.get(full_url, stream=True) as r:
         r.raise_for_status()
-        with local_filename.open('wb') as f:
+        with local_filename.open("wb") as f:
             for chunk in r.iter_content(chunk_size=chunk_size):
                 if chunk:  # Skip empty chunks
                     f.write(chunk)
-                    
+
     return str(local_filename)  # Return the file path as a string
 
 
-def main(url, base_url, download_link, test=False, dir='./data'):
+def main(url, base_url, download_link, test=False, dir="./data"):
     links = get_links(url, download_link)
     download_all_files(links, base_url, test, dir=dir)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Download data from the LOFAR surveys website")
-    parser.add_argument('--url', help="The URL of the page to scrape", default='https://lofar-surveys.org/dr2_release.html')
-    parser.add_argument('--base_url', help="The base URL of the website", default='https://lofar-surveys.org/')
-    parser.add_argument('--download_link', help="The regex pattern to match the download link", default='^public/DR2/mosaics/.*/mosaic-blanked.fits$')
-    parser.add_argument('--test', action='store_true', help="Only try to download the first file")
-    parser.add_argument('--dir', help="The local directory in which to save the data.", default='./data')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download data from the LOFAR surveys website"
+    )
+    parser.add_argument(
+        "--url",
+        help="The URL of the page to scrape",
+        default="https://lofar-surveys.org/dr2_release.html",
+    )
+    parser.add_argument(
+        "--base_url",
+        help="The base URL of the website",
+        default="https://lofar-surveys.org/",
+    )
+    parser.add_argument(
+        "--download_link",
+        help="The regex pattern to match the download link",
+        default="^public/DR2/mosaics/.*/mosaic-blanked.fits$",
+    )
+    parser.add_argument(
+        "--test", action="store_true", help="Only try to download the first file"
+    )
+    parser.add_argument(
+        "--dir", help="The local directory in which to save the data.", default="./data"
+    )
     args = parser.parse_args()
 
     main(args.url, args.base_url, args.download_link, args.test, args.dir)
